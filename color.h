80e8daf9851248a4af4f1bbcd4b18311b3e66bba
AA-HAMZA <ahmed.rk30@gmail.com>
2021-10-13 14:37:05 +0200

    Added multi-sampled pixels, camera object, world

--- color.h
+++ color.h
@@ -2,12 +2,27 @@
 #define COLOR_H
 
 #include "vec3.h"
+#include "rtweekend.h"
+
 #include <iostream>
 
-void write_color(std::ostream &out, color pixel_color) {
-    out << static_cast<int>(255.999 * pixel_color.x()) << " "
-        << static_cast<int>(255.999 * pixel_color.y()) << " "
-        << static_cast<int>(255.999 * pixel_color.z()) << "\n";
+void write_color(std::ostream &out, const color pixel_color, int samples_per_pixel) {
+    double r = pixel_color.x();
+    double g = pixel_color.y();
+    double b = pixel_color.z();
+
+    r /= static_cast<double>(samples_per_pixel);
+    g /= static_cast<double>(samples_per_pixel);
+    b /= static_cast<double>(samples_per_pixel);
+    /*
+    auto scale = 1.0 / samples_per_pixel;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+    */
+    out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << " "
+        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << " "
+        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << "\n";
 }
 
 #endif
