0bbb71c6a725c38d1e5fe03ad36c5ca2a0b53236
AA-HAMZA <ahmed.rk30@gmail.com>
2021-12-16 21:28:59 +0200

    Added new materials

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # RayTracing
 
 This is a very basic raytracer and it is for learning purposes.
+This project is still in progress.
 Followed this guide
 [_Ray Tracing in One Weekend_](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
--- material.h
+++ material.h
@@ -48,12 +48,25 @@ class dielectric: public material {
             double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
 
             vec3 unit_direction = unit_vector(r_in.direction());
-            vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
-            scattered = ray(rec.p, refracted);
+            double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+            double sin_theta = sqrt(1.0-cos_theta*cos_theta);
+
+            bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+            vec3 direction;
+            if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double())
+                direction = reflect(unit_direction, rec.normal);
+            else
+                direction = refract(unit_direction, rec.normal, refraction_ratio);
+            scattered = ray(rec.p, direction);
             return true;
         }
 
+    private:
         double ir;
-
+        static double reflectance(double cosine, double ref_idx) {
+            auto r0 = (1-ref_idx) / (1+ref_idx);
+            r0 = r0*r0;
+            return r0 + (1-r0)*pow((1-cosine), 5);
+        }
 };
 #endif
