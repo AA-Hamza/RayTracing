21d10c876ea3bbb47aef419daa95b0467aaa0bb6
AA-HAMZA <ahmed.rk30@gmail.com>
2022-01-23 19:28:03 +0200

    Added thread pool, poistionable camera, defocus

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # RayTracing
-
 This is a very basic raytracer and it is for learning purposes.
 This project is still in progress.
+
+## Using C++ threads to improve performance
+Used this library to make a thread pool [](https://github.com/vit-vit/CTPL)
+
+### Basic config? 
+    * Modify `samples_per_pixel` and `max_depth` with your prefered values, less values means better performance.
+    * Modify `image_width` and `aspect_ratio` with your prefered values.
+    * `num_of_threads` is the number of threads in the pool, choose a suitable number (Default: All available)
+    * `lines_per_thread` is the number of lines that each thread should handle (line is just a single row of pixels of the image width)
+
+### How to use?
+Compile using your favourite c++ compiler and if it has an option for an optimizer use it, `O3` for g++. also notice that this would make your CPU an inefficient heater, Mine got to 95C. and if you didn't use the optimizer it would take ages to finish.
+Redirect STDOUT to a file with .ppm extension then open it using an image viewer. it should look like this
+![](image.jpeg)
+
+
 Followed this guide
 [_Ray Tracing in One Weekend_](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include "sphere.h"
 #include "camera.h"
 #include "material.h"
+#include "ctpl_stl.h"
+#include <vector>
+#include <future>
 
 #include <iostream>
 
@@ -18,7 +21,6 @@ color ray_color(const ray& r, const hittable& world, int depth) {
         color attenuation;
         if (rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
             return attenuation * ray_color(scattered, world, depth-1);
-            //return 0.5 * ray_color(scattered, world, depth-1);
         }
         return color(0, 0, 0);
     }
@@ -27,47 +29,126 @@ color ray_color(const ray& r, const hittable& world, int depth) {
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
-int main()
-{
-    //Image 
-    const auto aspect_ratio = 16.0/9.0;
-    const int image_width = 400; 
-    const int image_height = static_cast<int>(image_width/aspect_ratio);
-    const int samples_per_pixel = 20;
-    const int max_depth = 8;
-
-    //World
+hittable_list random_scene() {
     hittable_list world;
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<dielectric>(1.5);
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   -0.4, material_left));
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
-    //Camera
-    camera cam;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
 
-    //Renderer
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
 
-    for (int j = image_height-1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            color pixel_color(0, 0, 0);
-            for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = double(i + random_double()) / (image_width-1);
-                auto v = double(j + random_double()) / (image_height-1);
-                ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world, max_depth);
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
             }
-            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
+int main()
+{
+    // Image
+    const auto aspect_ratio = 3.0 / 2.0;
+    const int image_width = 1200;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Modify thigs
+    const int samples_per_pixel = 20;
+    const int max_depth = 10;
+
+
+    // World
+    auto  world = random_scene();
+
+
+    // Camera
+    point3 lookfrom(13,2,3);
+    point3 lookat(0,0,0);
+    vec3 vup(0,1,0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
+
+    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+
+
+    // Threading
+    const int num_of_threads = std::thread::hardware_concurrency()/2+1;         //8 Threads in my case
+    const int num_of_jobs= num_of_threads * 4;          // that means if all jobs are equal then each thread would work 4 times, but they aren't equal so this is better
+    const int lines_per_thread = image_height/num_of_jobs; 
+    ctpl::thread_pool pool(num_of_threads);
+    std::vector<std::future<void>> jobs_results;
+
+    color **image_buffer = new color*[image_height];
+    for (int i = 0; i < image_height; ++i) {
+        image_buffer[i] = new color[image_width];
+    }
+
+    int lines_finished = 0;
+    int starting_line = image_height-1;
+    while (starting_line >= 0) {
+        int finishing_line = starting_line-lines_per_thread+1;
+        if (finishing_line < 0)
+            finishing_line = 0;
+        jobs_results.push_back(pool.push([=, &lines_finished](int) {
+            for (int j = starting_line; j >= finishing_line; --j) {
+                for (int i = 0; i < image_width; ++i) {
+                    color pixel_color(0, 0, 0);
+                    for (int s = 0; s < samples_per_pixel; ++s) {
+                        auto u = double(i + random_double()) / (image_width-1);
+                        auto v = double(j + random_double()) / (image_height-1);
+                        ray r = cam.get_ray(u, v);
+                        pixel_color += ray_color(r, world, max_depth);
+                    }
+                    image_buffer[j][i] = pixel_color;
+                }
+                lines_finished += 1;
+                std::cerr << "\rFinished: " << (lines_finished*100)/image_height << "%";
+            }
+        } ));
+        starting_line -= lines_per_thread;
+    }
+    for (int i = 0; i < jobs_results.size(); ++i) {
+        jobs_results[i].get();          //Wait for all jobs to be done first
+    }
+
+
+    // Writing to STDOUT
+    std::cerr << "\nWriting to stdout" << std::endl;
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    for (int j = image_height-1; j >= 0; --j) {
+        for (int i = 0; i < image_width; ++i) {
+            write_color(std::cout, image_buffer[j][i], samples_per_pixel);
+        }
+    }
     std::cerr << "\nDone.\n";
 }
